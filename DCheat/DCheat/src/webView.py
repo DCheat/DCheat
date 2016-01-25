09fc17531f3b714ca75465bdeaf5e133a6e168a5
dollking <dollking@users.noreply.github.com>
2016-01-25 11:20:01 +0900

    modify load func

--- DCheat/DCheat/src/webView.py
+++ DCheat/DCheat/src/webView.py
@@ -7,19 +7,19 @@
     :copyright: Hwang Sek-jin
 """
 
-from PyQt5 import QtWidgets
+from PyQt5 import uic
 from PyQt5 import QtGui
 from PyQt5 import QtCore
-from PyQt5 import uic
+from PyQt5 import QtWidgets
 from PyQt5.QtCore import *
-from DCheat import config
 from PyQt5.QtWebKit import *
+from DCheat import config
 
 class webView(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         QtWidgets.QMainWindow.__init__(self, parent)
-        self.ui = uic.loadUi(config.ROOT_PATH + 'view/webView.ui', self)
+        self.ui = uic.loadUi(config.config.ROOT_PATH + 'view/webView.ui', self)
 
-        self.ui.QWebView.load('http://www.kookmin.ac.kr')
+        self.ui.webView.load(QUrl('http://www.kookmin.ac.kr'))
 
         self.ui.show()
