a3a00633e733d5ce083cce118387702529e4f5d2
LLljs <hikoukigumo@naver.com>
2016-03-04 15:48:36 +0900

    message added

--- DCheat_Server/DCheat_Server/messageHandler.py
+++ DCheat_Server/DCheat_Server/messageHandler.py
@@ -6,8 +6,9 @@ class ForkingRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         from DCheat_Server.utils.selectQuery import select_unfinished_test_course_for_user,\
                                             select_user_index,\
-                                            select_ban_list_in_test,\
-                                            select_allow_site_index
+                                            select_course_index,\
+                                            select_allow_list_index,\
+                                            select_ban_list_index
         # Echo the back to the client
         data = self.request.recv(4096)
         data = data.decode()
@@ -17,6 +18,8 @@ class ForkingRequestHandler(socketserver.BaseRequestHandler):
         print("AAAAA")
         if data.find("SIN") != -1:
             login_handler(data)
+        elif data.find("SCS") != -1:
+            select_course(data)
         #if data.find("SCS") != -1:
         #    testIndex = int(data.split()[0])
         #    programIndexList = select_ban_list_in_test(testIndex)
@@ -34,6 +37,18 @@ class ForkingRequestHandler(socketserver.BaseRequestHandler):
         courseList = str(courseList).strip('[]').replace(' ', '')
         sendData = userIndex+"^"+courseList
         return sendData
+    
+    def select_course(self, data):
+        courseName = data.split(":")[2]
+        sendData = ''
+        courseIndex = select_course_index(courseName)
+        siteIndexList = select_allow_list_index(courseIndex)
+        siteIndexList = str(siteIndexList).strip('[]').replace(' ','').replace('(','').replace(',)','')
+        programIndexList = select_ban_list_index(courseIndex)
+        programIndexList = str(programIndexList).strip('[]').replace(' ','').replace('(','').replace(',)','')
+        sendData = programIndexList+"^"+siteIndexList
+        return sendData
+    
     def sign_up_handler():
         return
     def user_allow_ban_list_handler():
