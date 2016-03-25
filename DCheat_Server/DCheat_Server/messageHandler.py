0a29b215c296fd6f8657fe8d5766fed32cd25664
LLljs <hikoukigumo@naver.com>
2016-03-25 13:23:32 +0900

    update handler - dao.commit() added

--- DCheat_Server/DCheat_Server/messageHandler.py
+++ DCheat_Server/DCheat_Server/messageHandler.py
@@ -233,6 +233,7 @@ class ForkingRequestHandler(socketserver.BaseRequestHandler):
                     dao.commit()
                     userIndex = select_user_index(userInfo[0])
                 dao.add(insert_user_in_course(testIndex, userIndex))
+                dao.commit()
         except:
             self.request.send("0".encode('utf-8'))
             return
