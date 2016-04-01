aa9f533ff1c469c8b41141d547563902a4a154e5
황석진 <dollking2000@naver.com>
2016-04-01 16:16:07 +0900

    del celery pack

--- DCheat_Server/DCheat_Server/celeryServer/celery.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-
-app = Celery('celeryServer',
-             broker='amqp://guest:guest@localhost:5672',
-             include=['.task'])
-
-if __name__ == '__main__':
-    app.start()
